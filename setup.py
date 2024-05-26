c88b782ebe7c37488eab79cc6c4fc1180f412209
JAzco4 <115545051+Jimmys-Code@users.noreply.github.com>
2024-05-26 15:38:08 +0800

    fixed setup

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='rootify',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     description='A simple package to change the working directory to the project root based on a marker.',
     author='Jimmys-Code',
     author_email='jazcogames@gmail.com',
-    url='https://github.com/jimmys-code/updir',  # Replace with your GitHub URL or project page
+    url='https://github.com/jimmys-code/rootify',  # Replace with your GitHub URL or project page
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
